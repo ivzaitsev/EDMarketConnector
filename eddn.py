08c43c553c19f9cdee810b2d51437407034c67d1
Jonathan Harris <jonathan@marginal.org.uk>
2015-06-05 18:05:24 +0100

    Use version 2 of the EDDN schema.

--- eddn.py
+++ eddn.py
@@ -1,5 +1,4 @@
 # Export to EDDN
-# -*- coding: utf-8 -*-
 
 import json
 import numbers
@@ -12,50 +11,41 @@ from config import applongname, appversion, config
 from companion import categorymap, commoditymap, bracketmap
 
 upload = 'http://eddn-gateway.elite-markets.net:8080/upload/'
-schema = 'http://schemas.elite-markets.net/eddn/commodity/1'
+schema = 'http://schemas.elite-markets.net/eddn/commodity/2'
 
-def export(data, callback):
-
-    callback('Sending data to EDDN...')
+def export(data):
 
     querytime = config.getint('querytime') or int(time.time())
 
-    header = { 'softwareName': '%s [%s]' % (applongname, platform=='darwin' and "Mac OS" or system()),
-               'softwareVersion': appversion,
-               'uploaderID': data['commander']['name'].strip() }
-    systemName = data['lastSystem']['name'].strip()
-    stationName = data['lastStarport']['name'].strip()
-    timestamp = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(querytime))
-
-    # route all requests through a session in the hope of using keep-alive
-    session = requests.Session()
-    session.headers['connection'] = 'keep-alive'	# can help through a proxy?
-
-    commodities = data['lastStarport']['commodities']
-    i=0
-    for commodity in commodities:
-        i = i+1
-        callback('Sending %d/%d' % (i, len(commodities)))
+    commodities = []
+    for commodity in data['lastStarport']['commodities']:
         if isinstance(commodity.get('demandBracket'), numbers.Integral) and commodity.get('categoryname') and categorymap.get(commodity['categoryname'], True):
-            msg = { '$schemaRef': schema,
-                    'header': header,
-                    'message': {
-                        'systemName': systemName,
-                        'stationName': stationName,
-                        'itemName': commoditymap.get(commodity['name'].strip(), commodity['name'].strip()),
-                        'buyPrice': int(commodity.get('buyPrice', 0)),
-                        'stationStock': commodity.get('stockBracket') and int(commodity.get('stock', 0)) or 0,
-                        'sellPrice': int(commodity.get('sellPrice', 0)),
-                        'demand': commodity.get('demandBracket') and int(commodity.get('demand', 0)) or 0,
-                        'timestamp': timestamp,
-                    }
-                }
+            commodities.append({
+                'name'      : commoditymap.get(commodity['name'].strip(), commodity['name'].strip()),
+                'buyPrice'  : int(commodity.get('buyPrice', 0)),
+                'supply'    : commodity.get('stockBracket') and int(commodity.get('stock', 0)) or 0,
+                'sellPrice' : int(commodity.get('sellPrice', 0)),
+                'demand'    : commodity.get('demandBracket') and int(commodity.get('demand', 0)) or 0,
+            })
             if commodity.get('stockBracket'):
-                msg['message']['supplyLevel'] = bracketmap.get(commodity['stockBracket'])
+                commodities[-1]['supplyLevel'] = bracketmap.get(commodity['stockBracket'])
             if commodity.get('demandBracket'):
-                msg['message']['demandLevel'] = bracketmap.get(commodity['demandBracket'])
+                commodities[-1]['demandLevel'] = bracketmap.get(commodity['demandBracket'])
 
-            r = session.post(upload, data=json.dumps(msg))
-            r.raise_for_status()
+    msg = {
+        '$schemaRef' : schema,
+        'header'     : {
+            'uploaderID'      : data['commander']['name'].strip(),
+            'softwareName'    : '%s [%s]' % (applongname, platform=='darwin' and "Mac OS" or system()),
+            'softwareVersion' : appversion,
+        },
+        'message'    : {
+            'systemName'  : data['lastSystem']['name'].strip(),
+            'stationName' : data['lastStarport']['name'].strip(),
+            'timestamp'   : time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(querytime)),
+            'commodities' : commodities,
+            }
+    }
 
-    session.close()
+    r = requests.post(upload, data=json.dumps(msg))
+    r.raise_for_status()
